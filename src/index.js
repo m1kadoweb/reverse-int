6c1ccfc9c524be06fd3d06aba207f9334f4ef128
m1kadoweb <37939676+m1kadoweb@users.noreply.github.com>
2020-09-30 11:31:37 +0500

    refactor: improve code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
 module.exports = function reverse (n) {
-    let rev = n.toString().split("").reverse().join("");
-
-    return parseInt(rev);
+    return parseInt(n.toString().split("").reverse().join(""))
 }
